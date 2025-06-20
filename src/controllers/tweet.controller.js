32d749175f248a2b41f4d2affd550e8fc80913ea
Anupam Vinay <anupamvinay05@gmail.com>
2025-06-21 03:12:09 +0530

    some tweet commands

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,26 +1,51 @@
 import mongoose, { isValidObjectId } from "mongoose"
-import {Tweet} from "../models/tweet.model.js"
-import {User} from "../models/user.model.js"
+import { Tweet } from "../models/tweet.models.js"
+import { User } from "../models/user.models.js"
 import { ApiError } from "../utils/Apierror.js"
+import { ApiResponse } from "../utils/apiresponse.js"
 
 
 const createTweet = asyncHandler(async (req, res) => {
-    const content=req.body
+    const {content}=req.body
     if(!content)
         throw new ApiError(403,"no content found")
-    const user=req.user
+    const user=req.user._id
     
     const newTweet=await Tweet.create({
-
+        content,
+        owner:user
     })
+    if(!newTweet)
+        throw new ApiError(409,"error while creating tweet")
+    return res
+    .status(200)
+    .json(new ApiResponse(200,{newTweet},"tweet created successfully"))
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
+    
+    const user=req.user
+    let tweets=await Tweet.aggregate(
+        [
+           { $match:{
+                user:user._id
+            }
+        }
+        ]
+    )
+    if(!tweets)
+    {
+        throw new ApiError(404,"not bale to find tweets")
+    }
+    return res
+    .status(202)
+    .json(new ApiResponse(202,tweets,"done"))
+
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
-    //TODO: update tweet
+    
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
